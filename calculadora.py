feff5a59493cbc554516e664cd885ec02a470878
Tatiane Lima <tatiane.lima@contaazul.com>
2019-01-12 11:27:06 -0200

    refactoring and redesign of list
    now the list is showing like an math operation

--- calculadora.py
+++ calculadora.py
@@ -2,93 +2,93 @@ import os
 
 ########## Variável ##########
 result = 0
+equacao = ""
+operat = ""
 
 ########## funções ##########
 
 def show_help():
     clear_screen()
-    print("Para escolher a operação desejada digite um dos números abaixo:")
-    print("> Soma: +")
-    print("> Subtração: -")
-    print("> Multiplicação: *")
-    print("> Divisão: /")
     print("> Resultado final: =")
     print("> Ajuda: 'HELP'")
 
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
-def conta(num1, operat, num2):
+def conta(num1, operacao, num2):
     num1 = float(num1)
     num2 = float(num2)
-    if operation == "+":
-        result = num1 + num2
-    elif operation == "-":
-        result = num1 - num2
-    elif operation == "*":
-        result = num1 * num2
-    elif operation == "/":
-        result = num1 / num2
-    else:
-        print("Erro: conta recebeu uma operação não conhecida")
-    print("👉 {} {} {} = {}".format(num1, operation, num2, result))
-    return result
+    if operacao == "+":
+        return num1 + num2
+    elif operacao == "-":
+        return num1 - num2
+    elif operacao == "*":
+        return num1 * num2
+    elif operacao == "/":
+        return num1 / num2
 
 def show_result():
-    print("\n 👉", "Seu resultado final é {}\n👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋 \n".format(result))
-    attempt_count = 0
+    print("\n 👉", "Seu resultado final é {}\n \n👋👋👋👋👋👋👋👋👋👋👋👋👋👋👋 \n".format(result))
+
+def inserit_in_equacao(to_add, local): 
+        clear_screen()
+        local = "{} {} _".format(local[:-1], to_add)
+        print("\n")
+        print(local, "\n")
+        return local
 
 ############ inicio do programa ############ 
 
-print("\n👋 Bem vindo(a) à cauculadora da Tati ")
-
 show_help()
 
-while True: #primeira conta
-    input1 = input('\n🧮 Qual operação deseja fazer? ')
-    
-    if input1.upper() == "HELP":
-        show_help()
-        continue
+equacao = "_"
+print(equacao)
 
-    elif input1 == "+" or input1 == "-" or input1 == "/" or input1 == "*":
-        operation = input1
-        first_n = input("🔢 Primeiro número: ")
-        second_n = input("🔢 Segundo número: ")
-        if has_alphabet(first_n) == True or has_alphabet(second_n) == True:
-            print("🚷 Por favor, digite um número.")
-    
-        else:
-            result = conta(first_n, operation, second_n)
-            break
-
-    else:
-        clear_screen()
-        print("Digite a operação: + - * /")
-        continue
-        
-while True: #contas sequentes
-    others_inputs = input('• Número ou sinal: ')
+while True:
     try: 
-        if others_inputs.upper() == "HELP":
-            show_help()
-            continue
+        if len(equacao) < 3: #primeira conta
+            num = input("digite um numero: ")
+            equacao = inserit_in_equacao(num, equacao)
+            result = num
 
-        elif others_inputs == "+" or others_inputs == "-" or others_inputs == "/" or others_inputs == "*":
-            operation = others_inputs
-            second_n = input("• Segundo número: ")
-            result = conta(result, operation, second_n)
-            continue
-            
-        # elif has_alphabet(others_inputs) == True:
-        #     print("🚷 Por favor, digite números, sinais matematicos ou os comandos descritos no 'HELP.'")
-        
-        elif others_inputs == "=":
-            show_result()
-            break
+            operat = input("digite + - * / : ")
+            equacao = inserit_in_equacao(operat, equacao)
+
+
+            if num.upper() == "HELP" or operat.upper() == "HELP" :
+                show_help()
+
+            elif num == "=" or operat == "=":
+                show_result()
+                break
+            else:    
+                float(num) # averiguando se foi digitado um numero
 
         else:
-            result = conta(result, operation, others_inputs)
+            if num.upper() == "HELP" or operat.upper() == "HELP" :
+                show_help()
+
+            elif num == "=" or operat == "=":
+                show_result()
+                break
+
+            else:    
+                num = input("digite um numero: ")
+                equacao = inserit_in_equacao(num, equacao)
+                result = conta(result, operat, num)
+                print("= {}".format(result))
+
+                same_operat = operat
+                operat = input("digite + - * / : ")
+                if operat == "+" or operat == "-" or operat == "*" or operat == "/": 
+                    equacao = inserit_in_equacao(operat, equacao)
+                
+                else: 
+                    equacao = inserit_in_equacao(same_operat, equacao)
+                    operat = same_operat
+
+                print("= {}".format(result))              
+
     except ValueError:
         print("🚷🚷 Por favor, digite números, sinais matematicos ou os comandos descritos no 'HELP'")
-
+        continue
